4aa38a0ca9d577d086225f4f530926b92d2e0d5b
hasimo <sulky.to@gmail.com>
2010-11-22 16:23:55 +0900

    link_to_url escapes automatically.

--- test/rails/overrides/spec/helpers/helpers_spec.rb
+++ test/rails/overrides/spec/helpers/helpers_spec.rb
@@ -7,7 +7,10 @@ describe Jpmobile::Helpers do
   it "docomo_guid_link_to が guid=ON を付けたリンクを生成すること" do
     docomo_guid_link_to("STRING", :controller => "filter", :action => "rawdata").should == %{<a href="/filter/rawdata?guid=ON">STRING</a>}
   end
-
+  it "docomo_guid_link_toがstrをhtml_safeかチェックし 自動的にhtml要素をエスケープする" do
+    docomo_guid_link_to("<>&", :controller => "filter", :action => "rawdata").should == %{<a href="/filter/rawdata?guid=ON">&lt;&gt;&amp;</a>}
+    docomo_guid_link_to("<>&".html_safe, :controller => "filter", :action => "rawdata").should == %{<a href="/filter/rawdata?guid=ON"><>&</a>}
+  end
   it "softbank_location_link_to がリンク先にパラメータを含んでいても正常に動作すること" do
     # http://d.hatena.ne.jp/mizincogrammer/20090123/1232702067
     softbank_location_link_to("STRING", :controller => "filter", :action => "rawdata", :p => "param").should == %{<a href="location:auto?url=http://test.host/filter/rawdata&p=param">STRING</a>}
